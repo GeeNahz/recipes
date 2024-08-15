0734a407c5b8dc3243e1670e166adbf30b855af5
Godswill Nasara <godswillnlm@gmail.com>
2024-08-16 00:27:24 +0100

    delete: remove package-lock and change location of signup page

--- frontend/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import {SignupForm} from '@/components/ui/signup'
-
-export default function Signup() {
-    return (
-        <div className="container mx-auto p-24 min-h-screen">
-            <SignupForm />
-        </div>
-    )
-}
