9af78bfa8072e1f31c51c6c1ecf01b2146b91dab
Godswill Nasara <godswillnlm@gmail.com>
2024-08-16 00:22:38 +0100

    feature: connect signup server action to make req to server

--- frontend/lib/actions/signup.ts
+++ frontend/lib/actions/signup.ts
@@ -1,16 +1,23 @@
 'use server'
 
 import { SignupFormSchema } from "@/lib/_definitions"
+import AuthService from '@/lib/_services/auth-service'
 
 export async function signup(_prevState: unknown, formData: FormData) {
-    let payload = Object.fromEntries(formData)
-    const validationResult = SignupFormSchema.safeParse({
-        ...payload,
-    })
-    if (!validationResult.success) {
-        return {
-            errors: validationResult.error.flatten().fieldErrors,
-        }
+  let payload = Object.fromEntries(formData)
+  const validationResult = SignupFormSchema.safeParse({
+    ...payload,
+  })
+  if (!validationResult.success) {
+    return {
+      errors: validationResult.error.flatten().fieldErrors,
     }
-    return { message: JSON.stringify(payload) }
+  }
+
+  try {
+    const res = await AuthService.signupUser(validationResult.data)
+    return { message: res.data?.message, status: res.status }
+  } catch (err: any) {
+    return { message: 'Unable to register user. Please try again', status: 400 }
+  }
 }
