6df3f74a31e5452e8dbf85dab45fea20b3a342f1
Godswill Nasara <godswillnlm@gmail.com>
2024-11-05 14:42:25 +0100

    feature: create new user routes
    Add route to retrieve single user using user id
    Add route to delete user

--- backend/apps/users/api.py
+++ backend/apps/users/api.py
@@ -13,6 +13,10 @@ from .schemas import UserOut, UserIn
 
 router = Router(auth=JWTAuth())
 
+unauthenticated_error = {
+    "message": "You do not have the required permission to perform this request."
+}
+
 
 @router.get('/', response=List[UserOut])
 def list_users(request):
@@ -20,8 +24,16 @@ def list_users(request):
 
 
 @router.get('/me', response={200: UserOut, 401: MessageSchema})
-def get_user(request):
-    user = request.user
+def get_current_user(request):
+    return request.user
+
+
+@router.get('/{user_id}', response={200: UserOut, 401: MessageSchema})
+def get_user(request, user_id: str):
+    user = get_object_or_404(User, pk=user_id)
+    if not (request.user.is_superuser or (request.user.id == user.id)):
+        return 401, unauthenticated_error
+
     return user
 
 
@@ -29,10 +41,21 @@ def get_user(request):
 def update_users(request, user_id: str, data: UserIn):
     user = get_object_or_404(User, pk=user_id)
     if not (request.user.is_superuser or (request.user.id == user.id)):
-        return 401, {'message': 'You do not have the required permission to perform this request.'}
+        return 401, unauthenticated_error
 
     for attr, value in data.dict(exclude_unset=True).items():
         setattr(user, attr, value)
     user.save()
 
     return user
+
+
+@router.delete('/')
+def delete_user(request, user_id: str):
+    user = get_object_or_404(User, pk=user_id)
+    if not (request.user.is_superuser or (request.user.id == user.id)):
+        return 401, unauthenticated_error
+
+    user.delete()
+
+    return 204
